53df117502962a1f40d03e791ab7127eb59af006
preksha bhalani <prekshabhalani2927@gmail.com>
2020-01-20 15:56:54 -1100

    example of switch case

---
+++ 18th_jan/switch1.php
@@ -0,0 +1,15 @@
+<!-- switch case -->
+<?php
+$variable = 'value';
+switch ($variable) {
+    case 'value':
+        echo "$variable";
+        break;
+    case 'fyt':
+        echo "condition two";
+        break;
+    default:
+        echo "default";
+        break;
+}
+?>
