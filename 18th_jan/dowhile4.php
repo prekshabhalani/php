52fc27839bc75637922f0f5f8719a76de1276656
preksha bhalani <prekshabhalani2927@gmail.com>
2020-01-20 15:31:23 -1100

    example of do while loop

---
+++ 18th_jan/dowhile4.php
@@ -0,0 +1,13 @@
+<?php
+do{
+echo "I execute at least for one time";
+}
+while(false);
+do{
+    echo "<br>second I execute at least for one time";
+    if($i == 0)
+    {exit;}
+    $i==0;
+}
+    while(true);
+?>
