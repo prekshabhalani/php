0e7a68063d0303adfe7af07dee8eb66016cabedc
preksha bhalani <prekshabhalani2927@gmail.com>
2020-01-20 15:55:32 -1100

    example of variable in php

---
+++ 18th_jan/variable.php
@@ -0,0 +1,29 @@
+<?php
+//string without any declaration
+$string = "hiii";
+$number = 10;
+$string1 = "hello";
+$noString = 20.1066;
+$bool = true;
+
+
+//one example
+echo $string." ".$number." ".$string1." ".$noString." ".$bool;
+$bool = 1;
+
+//second example
+echo "<br> $string $number $string1 $noString $bool";
+
+//third example //convert bool into the string
+$bool = 'hey iam now string ';
+echo $bool;
+
+//forth example
+echo $a = 12 + $number . "<br>"; //addition
+echo $string | $string1 .'<br>'; // explain this plz :this is a xor opration of hello and hii's binary no. hahahahaha!!!! :) cool
+
+//fifth example
+echo $bool = 1 + true;
+
+
+?>
