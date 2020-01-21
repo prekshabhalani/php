fb9f6a869fff5f906ae924344e391103d30d5673
preksha bhalani <prekshabhalani2927@gmail.com>
2020-01-20 15:33:00 -1100

    example of if-else and else if too

---
+++ 18th_jan/innerifelse.php
@@ -0,0 +1,26 @@
+<?php
+//7th exaple nested if
+if (true) {
+    if(false)
+    {}
+    else{echo 'inside it else';}
+} else {
+    echo 'hey';
+}
+//8th now if else with else if 
+if (false) {
+    # code...
+} 
+else if(true)
+{
+    echo '<br> i am first condition which is true';
+}
+else if(true)
+{
+    echo 'i am second true condition but that does not matter';
+}
+else 
+{
+    # code...
+}
+?>
