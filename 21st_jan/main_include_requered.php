ce5bc33664cf712862209b879a4f2102bfda09f9
preksha bhalani <prekshabhalani2927@gmail.com>
2020-01-23 01:17:34 -1100

    html code to take some value

--- 21st_jan/main_include_requered.php
+++ 21st_jan/main_include_requered.php
@@ -2,4 +2,10 @@
 <?php
     include ('name.php');
     require ('about.php');
+//    require 'aboutsd.php';
+include 'dhfg.php';  
+echo "<h1>age: $age </h1>";
+require 'aboutsd.php';
+echo "<h1>age: $age </h1> ";
+
 ?>
