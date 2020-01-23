8f86af301b81beaa61461f72cd7cb7c3bb8cc534
preksha bhalani <prekshabhalani2927@gmail.com>
2020-01-23 00:36:34 -1100

    html code for some files that contain input values

--- 22nd_jan/takeinput.php
+++ 22nd_jan/takeinput.php
@@ -1,6 +1,6 @@
-<form method="POST" action='#'>
-    <hr>
-    <p>Enter Number: </p>
-    <input type="number" name="enterNumber" min=0 >
-    <input type="submit" value='submit' > 
-</form>
+    <form method="POST" action='#'>
+        <hr>
+        <p>Enter Number: </p>
+        <input type="number" name="enterNumber" min=0 >
+        <input type="submit" value='submit' > 
+    </form>
